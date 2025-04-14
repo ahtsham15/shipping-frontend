78b6fe30d40765d61dda3a496dd1ca80462b7c48
Admin <new_user@Usmans-MacBook-Pro.local>
2025-04-14 17:09:36 +0500

    sru/app: add the hello world

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,8 @@ function App() {
 
   return (
     <>
-      <div>
+      <h1>Hello world</h1>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -40,7 +41,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   );
 }
